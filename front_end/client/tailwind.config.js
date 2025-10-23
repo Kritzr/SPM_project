f2a6761294e206a155631175bc599b7e2c08c4de
Krithika Ravishankar <kritstudy15@gmail.com>
2025-10-23 07:04:19 +0530

    initial commit

---
+++ front_end/client/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#6264a7',
+        secondary: '#464775',
+      }
+    },
+  },
+  plugins: [],
+}
